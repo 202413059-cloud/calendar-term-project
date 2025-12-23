f9fafc0a55b6ab8efeacfffd936c421cc970f11c
202413059-cloud <202413059@jbnu.ac.kr>
2025-12-23 18:57:51 +0900

    Create App.vue

---
+++ web/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { auth } from "./firebase";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+
+const provider = new GoogleAuthProvider();
+
+const login = async () => {
+  const result = await signInWithPopup(auth, provider);
+  console.log(result.user.uid);
+};
+
+const logout = async () => {
+  await signOut(auth);
+};
+</script>
+
+<template>
+  <button @click="login">Google Login</button>
+  <button @click="logout">Logout</button>
+</template>
