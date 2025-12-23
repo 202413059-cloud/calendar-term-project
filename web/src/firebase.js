6e16842015ce8fcfd09423d155a3e2eb8011e3ec
202413059-cloud <202413059@jbnu.ac.kr>
2025-12-23 21:34:24 +0900

    Update firebase.js

--- web/src/firebase.js
+++ web/src/firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from "firebase/app";   // ⭐ 주석 제거
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
